0d1954fd326719e449e5d754b1c3a1fb39982457
EmptySoulOfficial <79078117+EmptySoulOfficial@users.noreply.github.com>
2021-02-19 13:05:12 +0100

    Burgermenu ESV2 (hardcoded)

---
+++ burgermenu_esv2(hardcode-version)/README.md
@@ -0,0 +1,5 @@
+# Burgermenu ESV2 [V 2.2]
+(HARDCODE VERSION)
+
+Nice animated burgermenu with different styles
+### Created by Empty Soul
---
+++ burgermenu_esv2(hardcode-version)/data/burgermenu_esv2/burgermenu_esv2.js
@@ -0,0 +1,35 @@
+//burgermenu_esv2 by EMpty SOul [V 2.2 - REACT VERSION]
+
+
+
+
+function bmenu_esv2_click()
+{
+//get all id's//
+// bml = burger menu line //
+let bml0 = document.getElementById("bml_0");
+
+
+let bm_button = document.getElementById("bmesv2_button");
+//OPEN THE MENU UI-> let mmenu = document.getElementById("main_menu");
+
+
+if (bm_button.classList.contains("bmactive")) {
+/*--close--*/
+bm_button.classList.remove("bmactive");
+bm_button.classList.add("bmactive_re");
+$( ".burgermenu_line" ).removeClass( "bml_color_open" );
+
+}else{
+/*--open--*/
+bm_button.classList.add("bmactive");
+bm_button.classList.remove("bmactive_re");
+$('html').css("overflow","hidden");
+$( ".burgermenu_line" ).addClass( "bml_color_open" );
+}
+}
+
+
+
+
+
