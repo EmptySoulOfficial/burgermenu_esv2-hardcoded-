a974193843abea631fb2e7aa810c111583048d1c
EmptySoulOfficial <79078117+EmptySoulOfficial@users.noreply.github.com>
2021-02-19 15:53:58 +0100

    Release v2.3

--- burgermenu_esv2(hardcode-version)/data/burgermenu_esv2/burgermenu_esv2.js
+++ burgermenu_esv2(hardcode-version)/data/burgermenu_esv2/burgermenu_esv2.js
@@ -1,35 +1,22 @@
-//burgermenu_esv2 by EMpty Soul [V 2.2 hardcode-version]
+//burgermenu_esv2 by Steve - Empty Soul [V 2.3 hardcode-version]
 
+function bmenu_esv2_click() {
+  //get all id's//
+  // bml = burger menu line //
+  let bml0 = document.getElementById("bml_0");
 
+  let bm_button = document.getElementById("bmesv2_button");
+  //OPEN THE MENU UI-> let mmenu = document.getElementById("main_menu");
 
-
-function bmenu_esv2_click()
-{
-//get all id's//
-// bml = burger menu line //
-let bml0 = document.getElementById("bml_0");
-
-
-let bm_button = document.getElementById("bmesv2_button");
-//OPEN THE MENU UI-> let mmenu = document.getElementById("main_menu");
-
-
-if (bm_button.classList.contains("bmactive")) {
-/*--close--*/
-bm_button.classList.remove("bmactive");
-bm_button.classList.add("bmactive_re");
-$( ".burgermenu_line" ).removeClass( "bml_color_open" );
-
-}else{
-/*--open--*/
-bm_button.classList.add("bmactive");
-bm_button.classList.remove("bmactive_re");
-$('html').css("overflow","hidden");
-$( ".burgermenu_line" ).addClass( "bml_color_open" );
+  if (bm_button.classList.contains("bmactive")) {
+    /*--close--*/
+    bm_button.classList.remove("bmactive");
+    bm_button.classList.add("bmactive_re");
+    bml0.classList.remove("bml_color_open");
+  } else {
+    /*--open--*/
+    bm_button.classList.add("bmactive");
+    bm_button.classList.remove("bmactive_re");
+    bml0.classList.add("bml_color_open");
+  }
 }
-}
-
-
-
-
-
